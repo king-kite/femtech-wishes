24afec87339a8808cf4296d19fda44d283ee9da9
kingkite <kitewebdevelopers@gmail.com>
2022-12-23 16:05:00 +0100

    Next Oush First Page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="relative">
       <div className="bg-gray-100 max-w-[500px] mx-auto relative">
-        <div className="bg-background bg-white h-full min-h-[100vh] px-4 py-8 shadow-lg">
+        <div className="bg-background bg-white flex flex-col items-center justify-center h-full min-h-[100vh] px-4 py-8 shadow-lg">
           <div className="flex flex-col items-center justify-center">
             <div className="w-[250px]">
               <img className="h-full w-full" src="/images/logo.svg" alt="femtech" />
